72103d9fd4ff5af1e901e2aaca9b381f9df706dd
Panditnikhil001 <148411703+Panditnikhil001@users.noreply.github.com>
2023-10-19 11:21:59 +0530

    Add files via upload

---
+++ even or odd.java
@@ -0,0 +1,21 @@
+package loops;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any Number : ");
+        n = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= n; i++) {
+            if ((i&1) == 0) {
+                System.out.println(i+" is Even");
+            } else {
+                System.out.println(i+" is Odd");
+            }
+        }
+    }
+}
